224b14dd8983b5fab586852dad4146611c403b3c
Viacheslav Roganov <v.roganov@gmail.com>
2021-02-10 15:17:15 +0200

    add list reports

---
+++ newdlx/src/components/ListReports.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        maxWidth: 360,
+        backgroundColor: theme.palette.background.paper,
+    },
+}));
+
+// interface ListReportProps {
+//     reports: ReportProps[];
+// }
+// interface ReportProps {
+//     reportId: string;
+//     reportName: string;
+// }
+
+// function ListItemLink(props: any) {
+//     return <ListItem button component="a" {...props} />;
+// }
+
+export default function SimpleList(props: any) {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <List component="nav" aria-label="secondary mailbox folders">
+                {props.reports.map((el: any) => (
+                    <ListItem button>
+                        <ListItemText primary={el.reportName} onClick={() => props.setReport(el.reportId)} />
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
+}
